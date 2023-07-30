f84d1ed999d899b80613472cf552eeff6b5421ba
Ashish Sharma <60579703+code-guy-ashish@users.noreply.github.com>
2023-07-30 22:30:38 +0530

    first commit

---
+++ scripts/type_role.js
@@ -0,0 +1,8 @@
+
+
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Machine Learning Developer', 'Cyber Enthusiast'],
+    typeSpeed: 45,
+    loop: true,
+    loopCount: Infinity,
+});
